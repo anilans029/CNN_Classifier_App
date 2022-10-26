56d6bc21f45b3e6bcb0c1c787246a52fa0f2ebb6
anil <anilsai029@gmail.com>
2022-10-26 20:16:32 +0530

    removed the typo error

--- src/classifier_app/__init__.py
+++ src/classifier_app/__init__.py
@@ -5,7 +5,7 @@ import logging
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s:] %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok= True)
-log_filepath = os.paht.join(log_dir, "running_logs.log")
+log_filepath = os.path.join(log_dir, "running_logs.log")
 
 logging.basicConfig(
                     level=logging.INFO,
