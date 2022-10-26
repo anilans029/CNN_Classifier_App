241eb93378d76366f71ec2f4f99aa0d9f047e2c7
anil <anilsai029@gmail.com>
2022-10-27 00:29:07 +0530

    Stage_01 data ingestion completed in pipeline

---
+++ src/classifier_app/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from classifier_app.config import ConfigurationManager
+from classifier_app.components import DataIngestion
+from classifier_app import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+def main():
+    try:
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.unzip_and_clean()
+
+    except Exception as e:
+        raise e
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
