d924d7ad12bc505fd1c3e825b24a33cdcf99939a
anil <anilsai029@gmail.com>
2022-10-30 13:15:18 +0530

    train_test_split function added to the pipeline

--- src/classifier_app/pipeline/stage_01_data_ingestion.py
+++ src/classifier_app/pipeline/stage_01_data_ingestion.py
@@ -11,6 +11,7 @@ def main():
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.unzip_and_clean()
+        data_ingestion.train_test_split()
 
     except Exception as e:
         raise e
