654145a09e43c981a21fd76068e081f5243e69d0
anil <anilsai029@gmail.com>
2022-10-30 13:14:56 +0530

    data_ingestion config entity updated

--- src/classifier_app/entity/config_entity.py
+++ src/classifier_app/entity/config_entity.py
@@ -6,7 +6,12 @@ DataIngestionConfig = namedtuple("DataIngestionConfig",[
                             "root_dir",
                             "soruce_url",
                             "local_data_file",
-                            "unzip_dir"
+                            "unzip_dir",
+                            "train_dir_name",
+                            "test_dir_name",
+                            "split_data",
+                            "params_train_ratio",
+                            "params_test_ratio"
                                 ])
 
 
@@ -15,6 +20,7 @@ class PrepareBaseModelConfig:
     root_dir: Path
     base_model_path: Path
     updated_base_model_path: Path
+
     params_CLASSES: int
     params_WEIGHTS: str
     params_LEARNING_RATE: float
@@ -34,7 +40,9 @@ class TrainingConfig:
     trained_model_path: Path
     updated_base_model_path: Path
     training_data: Path
+    testing_data: Path
     params_epochs: int
     params_batch_size:int
     params_is_augmentation: bool
     params_image_size: list
+
